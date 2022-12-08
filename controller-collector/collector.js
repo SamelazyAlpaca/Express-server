64a49093f58e764f1f964d06cd749f031fc6463c
Mirzoyan Tigran <mirzoiantigran92@gmail.com>
2022-12-08 12:25:41 +0300

    removed unused file

--- controller-collector/collector.js
+++
@@ -1,8 +0,0 @@
-// const recursive = require('recursive-readdir-sync');
-import fs from 'fs'
-import path from 'path'
-const __dirname = path.resolve()
-import { recursive } from 'recursive-readdir-sync'
-
-recursive(`${__dirname}/routes`)
-	.forEach(file => app.use('/tasks', require(file)));
